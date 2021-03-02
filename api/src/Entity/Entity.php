fca8ea3bbdf52af3992eab15d0a9e5a20dfa27b3
Sarai Misidjan <smisidjan@roc-dev.com>
2021-03-02 13:10:49 +0100

    small fix

--- api/src/Entity/Entity.php
+++ api/src/Entity/Entity.php
@@ -3,23 +3,62 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\EntityRepository;
+use App\Repository\AttributeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass=EntityRepository::class)
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *     itemOperations={
+ *          "get",
+ *          "put",
+ *          "delete",
+ *          "get_change_logs"={
+ *              "path"="/emails/{id}/change_log",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Changelogs",
+ *                  "description"="Gets al the change logs for this resource"
+ *              }
+ *          },
+ *          "get_audit_trail"={
+ *              "path"="/emails/{id}/audit_trail",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Audittrail",
+ *                  "description"="Gets the audit trail for this resource"
+ *              }
+ *          }
+ *     },
+ *  collectionOperations={
+ *     "get",
+ *    "post"
+ *  })
+ * @ORM\Entity(repositoryClass=EntityRepository)
+ * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
  */
 class Entity
 {
     /**
+     * @var UuidInterface The UUID identifier of this Attribute.
+     *
+     * @Assert\Uuid
+     * @Groups({"read"})
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
     private $id;
 
-    public function getId(): ?int
+    public function getId()
     {
         return $this->id;
     }
