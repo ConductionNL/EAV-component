b061f3410a443f4ead1398e68dff3a807d174188
Wilco Louwerse <wilco@conduction.nl>
2021-03-02 12:38:56 +0100

    Basic stuff

---
+++ api/src/Entity/Value.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ValueRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ValueRepository::class)
+ */
+class Value
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
