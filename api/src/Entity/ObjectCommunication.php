7bf143ce909a3a544024fb8151b4f9e616c63d61
Wilco Louwerse <wilco@conduction.nl>
2021-03-16 19:22:55 +0100

    Changes/fixes, see EAV google doc documentation

--- api/src/Entity/ObjectCommunication.php
+++ api/src/Entity/ObjectCommunication.php
@@ -10,7 +10,15 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * Description
+ *
+ * @category Entity
+ *
+ * @ApiResource(
+ *  collectionOperations={
+ *  	"get",
+ *  	"post",
+ *  })
  * @ORM\Entity(repositoryClass="App\Repository\ObjectCommunicationRepository")
  */
 class ObjectCommunication
@@ -27,38 +35,26 @@ class ObjectCommunication
     private $id;
 
     /**
-     * @var string The type of action we are doing
-     *
-     * @example get
-     *
-     * @Assert\Choice({"get", "post", "put"})
+     * @var string (get, post and put) The component code for the objectEntity we are getting, creating or updating
      *
      * @Groups({"read"})
      * @ORM\Column(type="string", length=255)
      */
-    private $action = 'get';
+    private $componentCode = 'eav';
 
     /**
-     * @var string (get, post and put) The component for the objectEntity we are getting, creating or updating
+     * @var string (get, post and put) The entity name for the objectEntity we are getting, creating or updating. (this actually needs to be the second value of the entity type, so in case of entity type: cc/people, this would/should be people)
      *
      * @Groups({"read"})
      * @ORM\Column(type="string", length=255)
      */
-    private $component = 'eav';
-
-    /**
-     * @var string (get, post and put) The entity type for the objectEntity we are getting, creating or updating
-     *
-     * @Groups({"read"})
-     * @ORM\Column(type="string", length=255)
-     */
-    private $entityType;
+    private $entityName;
 
     /**
      * @var string (get or put) The uuid of the objectEntity we are getting or updating
      *
      * @Groups({"read"})
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="uuid", length=255, nullable=true)
      */
     private $objectEntityId;
 
@@ -67,23 +63,39 @@ class ObjectCommunication
         return $this->id;
     }
 
-    public function getAction(): ?string
+    public function getComponentCode(): ?string
     {
-        return $this->action;
+        return $this->componentCode;
     }
 
-    public function getComponent(): ?string
+    public function setComponentCode(string $componentCode): self
     {
-        return $this->component;
+        $this->componentCode = $componentCode;
+
+        return $this;
     }
 
-    public function getType(): ?string
+    public function getEntityName(): ?string
     {
-        return $this->entityType;
+        return $this->entityName;
+    }
+
+    public function setEntityName(string $entityName): self
+    {
+        $this->entityName = $entityName;
+
+        return $this;
     }
 
     public function getObjectEntityId(): ?string
     {
         return $this->objectEntityId;
+    }
+
+    public function setObjectEntityId(string $objectEntityId): self
+    {
+        $this->objectEntityId = $objectEntityId;
+
+        return $this;
     }
 }
