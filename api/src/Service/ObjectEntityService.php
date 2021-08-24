37f06e1aa16c365986fa3e69a3740e0a2deef022
Wilco Louwerse <wilco@conduction.nl>
2021-08-24 13:14:50 +0200

    clearer error messages

--- api/src/Service/ObjectEntityService.php
+++ api/src/Service/ObjectEntityService.php
@@ -479,7 +479,7 @@ class ObjectEntityService
         // Check if the value is null and if so if this is allowed or not
         if (is_null($bodyValue)) {
             if (!$attribute->getNullable()) {
-                throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
             }
         } else {
             // Check if attribute has an enum and if so check if the bodyValue equals one of the enumValues
@@ -489,63 +489,63 @@ class ObjectEntityService
                 } else {
                     $enumValues = '[' . implode( ", ", $attribute->getEnum() ) . ']';
                 }
-                throw new HttpException('Attribute: [' . $attribute->getName() . '] must be one of the following values: ' . $enumValues . ' !', 400);
+                throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be one of the following values: ' . $enumValues . ' !', 400);
             }
 
             // Do checks for attribute depending on its type-format
             switch ($typeFormat) {
                 case 'string-string':
                     if (!is_string($bodyValue)) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
                     }
                     if ($attribute->getMinLength() && strlen($bodyValue) < $attribute->getMinLength()) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] is to short, minimum length is ' . $attribute->getMinLength() . ' !', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] is to short, minimum length is ' . $attribute->getMinLength() . ' !', 400);
                     }
                     if ($attribute->getMaxLength() && strlen($bodyValue) > $attribute->getMaxLength()) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] is to long, maximum length is ' . $attribute->getMaxLength() . ' !', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] is to long, maximum length is ' . $attribute->getMaxLength() . ' !', 400);
                     }
                     break;
                 case 'number-number':
                     if (!is_integer($bodyValue) && !is_float($bodyValue) && gettype($bodyValue) != 'float' && gettype($bodyValue) != 'double') {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
                     }
                     break;
                 case 'integer-integer':
                     if (!is_integer($bodyValue)) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
                     }
                     if ($attribute->getMinimum()) {
                         if ($attribute->getExclusiveMinimum() && $bodyValue <= $attribute->getMinimum()) {
-                            throw new HttpException('Attribute: [' . $attribute->getName() . '] must be higher than ' . $attribute->getMinimum() . ' !', 400);
+                            throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be higher than ' . $attribute->getMinimum() . ' !', 400);
                         } elseif ($bodyValue < $attribute->getMinimum()) {
-                            throw new HttpException('Attribute: [' . $attribute->getName() . '] must be ' . $attribute->getMinimum() . ' or higher!', 400);
+                            throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be ' . $attribute->getMinimum() . ' or higher!', 400);
                         }
                     }
                     if ($attribute->getMaximum()) {
                         if ($attribute->getExclusiveMaximum() && $bodyValue >= $attribute->getMaximum()) {
-                            throw new HttpException('Attribute: [' . $attribute->getName() . '] must be lower than ' . $attribute->getMaximum() . ' !', 400);
+                            throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be lower than ' . $attribute->getMaximum() . ' !', 400);
                         } elseif ($bodyValue > $attribute->getMaximum()) {
-                            throw new HttpException('Attribute: [' . $attribute->getName() . '] must be ' . $attribute->getMaximum() . ' or lower!', 400);
+                            throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be ' . $attribute->getMaximum() . ' or lower!', 400);
                         }
                     }
                     if ($attribute->getMultipleOf() && $bodyValue % $attribute->getMultipleOf() != 0) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] must be a multiple of ' . $attribute->getMultipleOf() . ', ' . $bodyValue . ' is not a multiple of ' . $attribute->getMultipleOf() . ' !', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be a multiple of ' . $attribute->getMultipleOf() . ', ' . $bodyValue . ' is not a multiple of ' . $attribute->getMultipleOf() . ' !', 400);
                     }
                     break;
                 case 'boolean-boolean':
                     if (!is_bool($bodyValue)) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
                     }
                     break;
                 case 'array-array':
                     if (!is_array($bodyValue)) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', ' . gettype($bodyValue) . ' given!', 400);
                     }
                     if ($attribute->getMinItems() && count($bodyValue) < $attribute->getMinItems()) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] has to few items ( ' . count($bodyValue) . ' ), the minimum array length of this attribute is ' . $attribute->getMinItems() . ' !', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] has to few items ( ' . count($bodyValue) . ' ), the minimum array length of this attribute is ' . $attribute->getMinItems() . ' !', 400);
                     }
                     if ($attribute->getMaxItems() && count($bodyValue) > $attribute->getMaxItems()) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] has to many items ( ' . count($bodyValue) . ' ), the maximum array length of this attribute is ' . $attribute->getMaxItems() . ' !', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] has to many items ( ' . count($bodyValue) . ' ), the maximum array length of this attribute is ' . $attribute->getMaxItems() . ' !', 400);
                     }
                     if ($attribute->getUniqueItems() && count(array_filter(array_keys($bodyValue), 'is_string')) == 0) {
                         // TODO:check this in another way so all kinds of arrays work with it.
@@ -556,7 +556,7 @@ class ObjectEntityService
                             }
                         }
                         if (!$containsStringKey && count($bodyValue) !== count(array_unique($bodyValue))) {
-                            throw new HttpException('Attribute: [' . $attribute->getName() . '] must be an array of unique items!', 400);
+                            throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] must be an array of unique items!', 400);
                         }
                     }
                     break;
@@ -564,7 +564,7 @@ class ObjectEntityService
                     try {
                         new \DateTime($bodyValue);
                     } catch (HttpException $e) {
-                        throw new HttpException('Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', failed to parse string to DateTime!', 400);
+                        throw new HttpException('The entity type: [' . $attribute->getEntity()->getType() . '] Attribute: [' . $attribute->getName() . '] expects ' . $attribute->getType() . ', failed to parse string to DateTime!', 400);
                     }
                     break;
                 default:
